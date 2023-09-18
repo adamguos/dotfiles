6135e1382f7ab5bc4ac01e909bb576e15ea248fb
Adam Guo <agsmguo@gmail.com>
2023-09-17 23:24:12 -0400

    Revert "rose-pine-moon"
    This reverts commit 5dd7b984354b8332c10e0698bf4d426cccc3d079.

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,7 @@ vim.opt.showbreak = '>>  '
 vim.g.mapleader = ' '
 
 vim.opt.background = 'dark'
-vim.cmd.colorscheme 'rose-pine-moon'
+vim.cmd.colorscheme 'gruvbox'
 
 require('plugins')
 require('neo-tree-config')
