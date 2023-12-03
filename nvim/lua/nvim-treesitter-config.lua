26bdbffd2604e3a97f8a26d0e3a51e3c0f3a9a53
Adam Guo <agsmguo@gmail.com>
2023-12-02 21:44:44 -0500

    nvim-treesitter-config

---
+++ nvim/lua/nvim-treesitter-config.lua
@@ -0,0 +1,10 @@
+-- config_file_path=~/.config/nvim/lua/nvim-treesitter-config.lua
+
+require('nvim-treesitter.configs').setup({
+    highlight = {
+        enable = true
+    },
+    indent = {
+        enable = true
+    }
+})
