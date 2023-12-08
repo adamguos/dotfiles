bcfae4cd2f2bd988f764d3a653ad7bbdcd01660e
Adam Guo <agsmguo@gmail.com>
2023-12-08 15:47:12 -0500

    keybinds

--- nvim/lua/neo-tree-config.lua
+++ nvim/lua/neo-tree-config.lua
@@ -1,5 +1,5 @@
 -- config_file_path=~/.config/nvim/lua/neo-tree-config.lua
 
-vim.api.nvim_set_keymap('n', '<leader>t', ':Neotree position=left toggle reveal_force_cwd<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>t', ':Neotree position=float toggle reveal_force_cwd<CR>', {})
 
 vim.api.nvim_set_keymap('n', '<leader>r', ':lua require("buffer_manager.ui").toggle_quick_menu()<CR>', {})
