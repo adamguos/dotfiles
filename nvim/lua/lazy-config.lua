eca21af5e09e53ba3a62ee1710134fbffda771e4
Adam Guo <agsmguo@gmail.com>
2024-08-05 22:25:47 -0400

    everforest hard

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
         config = function()
             vim.o.background = 'dark'
             vim.g.everforest_enable_italic = true
-            vim.g.everforest_background = 'soft'
+            vim.g.everforest_background = 'hard'
             vim.g.everforest_better_performance = 1
             vim.cmd.colorscheme('everforest')
         end
