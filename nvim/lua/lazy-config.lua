7ac5f6bea8fe5a0409a6f5bb245a490c76a14348
Adam Guo <agsmguo@gmail.com>
2024-10-12 12:54:23 -0400

    vague

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -15,15 +15,12 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-        'sainnhe/everforest',
+        'vague2k/vague.nvim',
         lazy = false,
         priority = 1000,
         config = function()
             vim.o.background = 'dark'
-            vim.g.everforest_enable_italic = true
-            vim.g.everforest_background = 'hard'
-            vim.g.everforest_better_performance = 1
-            vim.cmd.colorscheme('everforest')
+            vim.cmd.colorscheme('vague')
         end
     },
     {
