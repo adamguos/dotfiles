c202c09ec197b717c3592b6f791440add2c029ec
Adam Guo <agsmguo@gmail.com>
2024-11-06 12:39:32 -0500

    zenburned

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
         priority = 1000,
         config = function()
             vim.o.background = 'dark'
-            vim.cmd.colorscheme('duckbones')
+            vim.cmd.colorscheme('zenburned')
         end
     },
     {
