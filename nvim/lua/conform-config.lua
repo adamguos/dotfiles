c6d77516f94ca77bfd10cdd8c3e44459dbceccbf
Adam Guo <agsmguo@gmail.com>
2025-03-15 11:38:39 -0400

    conform

--- nvim/lua/conform-config.lua
+++ nvim/lua/conform-config.lua
@@ -3,7 +3,9 @@ local conform = require('conform')
 conform.setup({
     formatters_by_ft = {
         python = { "isort", "black" },
+        cpp = { "clang-format" }
     },
+    format_on_save = {}
 })
 
 vim.keymap.set('n', '<space>nf', function()
