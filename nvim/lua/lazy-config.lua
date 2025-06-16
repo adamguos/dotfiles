b0a91a2e9ec32c10ec576ee7530e260441972ce8
Adam Guo <agsmguo@gmail.com>
2025-06-15 22:58:34 -0400

    kanagawa wave

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
         priority = 1000,
         config = function()
             vim.o.background = 'dark'
-            vim.cmd.colorscheme('kanagawa-dragon')
+            vim.cmd.colorscheme('kanagawa-wave')
         end
     },
     {
