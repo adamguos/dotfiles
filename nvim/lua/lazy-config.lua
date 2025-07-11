8fa49a7daf65802cc7144d4642c137c74bf35fb1
Adam Guo <agsmguo@gmail.com>
2025-07-10 22:51:00 -0400

    kanso

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -13,12 +13,12 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-        'EdenEast/nightfox.nvim',
+        'webhooked/kanso.nvim',
         lazy = false,
         priority = 1000,
         config = function()
             vim.o.background = 'dark'
-            vim.cmd.colorscheme('terafox')
+            vim.cmd.colorscheme('kanso')
         end
     },
     {
