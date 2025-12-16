ab36afa279a3d475c05a48084dc8ced344be5c32
Adam Guo <agsmguo@gmail.com>
2025-12-15 22:19:32 -0500

    oasis-desert

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -67,7 +67,7 @@ require("lazy").setup({
         priority = 1000,
         config = function()
             vim.o.background = "dark"
-            vim.cmd.colorscheme("oasis-mirage")
+            vim.cmd.colorscheme("oasis-desert")
         end
     },
     {
