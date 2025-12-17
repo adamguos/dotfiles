d0c27c79f85025f7c05f40511f25a12b00b41d00
Adam Guo <agsmguo@gmail.com>
2025-12-16 23:24:10 -0500

    oasis-rose

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -67,7 +67,7 @@ require("lazy").setup({
         priority = 1000,
         config = function()
             vim.o.background = "dark"
-            vim.cmd.colorscheme("oasis-desert")
+            vim.cmd.colorscheme("oasis-rose")
         end
     },
     {
