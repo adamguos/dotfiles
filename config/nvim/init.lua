0a5111cc1be0e75dd98b06ab99e4948dd955ffe5
Adam Guo <agsmguo@gmail.com>
2026-03-16 22:29:55 -0400

    oasis-abyss

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -71,7 +71,7 @@ require("lazy").setup({
         priority = 1000,
         config = function()
             vim.o.background = "dark"
-            vim.cmd.colorscheme("oasis-cactus")
+            vim.cmd.colorscheme("oasis-abyss")
         end
     },
     {
