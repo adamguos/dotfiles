ff154bc048816e9850cf25e7ea02e085c6c251d7
Adam Guo <agsmguo@gmail.com>
2026-07-30 18:21:36 -0400

    dark

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -71,7 +71,7 @@ require("lazy").setup({
         lazy = false,
         priority = 1000,
         config = function()
-            vim.o.background = "light"
+            vim.o.background = "dark"
             vim.cmd.colorscheme("rose-pine")
         end
     },
