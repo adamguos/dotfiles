b3c8c56947e3276249a80fc4babe172d7a00ce2b
Adam Guo <agsmguo@gmail.com>
2026-07-31 22:48:38 -0400

    oasis-sol

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -66,13 +66,12 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
+        "uhs-robert/oasis.nvim",
         lazy = false,
         priority = 1000,
         config = function()
             vim.o.background = "dark"
-            vim.cmd.colorscheme("rose-pine")
+            vim.cmd.colorscheme("oasis-sol")
         end
     },
     {
