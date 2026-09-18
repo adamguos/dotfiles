69e554e251bbb376d7db9e21b9e0f594ce010597
Adam Guo <agsmguo@gmail.com>
2026-09-18 16:50:23 -0400

    oasis-sol

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -66,13 +66,13 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-        "samharju/synthweave.nvim",
+        "uhs-robert/oasis.nvim",
         build = false,
         lazy = false,
         priority = 1000,
         config = function()
             vim.o.background = "dark"
-            vim.cmd.colorscheme("synthweave")
+            vim.cmd.colorscheme("oasis-sol")
         end
     },
     {
